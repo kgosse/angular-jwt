91c5bac16f97241cf73a36a0eb8aa0a01ae5703a
Kevin Gosse <gossekevin1@gmail.com>
2016-01-08 13:01:25 +0100

    add the authentication form

--- public/main.js
+++ public/main.js
@@ -8,16 +8,42 @@ var app = angular.module("app", []);
 
 app.constant('API_URL', 'http://localhost:3000');
 
-app.controller('MainCtrl', function(RandomUserFactory){
+app.controller('MainCtrl', function(RandomUserFactory, UserFactory){
     var vm = this;
     vm.getRandomUser = getRandomUser;
+    vm.login = login;
 
     function getRandomUser(){
         RandomUserFactory.getUser().then((response)=>{
             vm.randomUser = response.data;
         });
     }
+
+    function login(username, password){
+        UserFactory.login(username, password).then(function success(response){
+            vm.user = response.data;
+        }, handleError);
+    }
+
+    function handleError(response){
+        alert('Error' + response.data);
+    }
 });
+
+app.factory('UserFactory', function($http, API_URL){
+    'use strict';
+
+    return {
+        login
+    };
+
+    function login(username, password){
+        return $http.post(API_URL + '/login', {
+            username,
+            password
+        });
+    }
+})
 
 app.factory('RandomUserFactory', function($http, API_URL){
     return {
--- server.js
+++ server.js
@@ -5,9 +5,17 @@
 var express = require('express');
 var faker = require('faker');
 var cors = require('cors');
+var bodyParser = require('body-parser');
+
+var user = {
+    username: 'kgosse',
+    password: 'pass'
+};
+
 
 var app = express();
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/random-user', function(req, res){
     var user = faker.helpers.userCard();
@@ -15,4 +23,19 @@ app.get('/random-user', function(req, res){
     res.json(user);
 });
 
+app.post('/login', authenticate, function(req, res){
+    res.send(user);
+});
+
 app.listen(3000, ()=>console.log('App listening on localhost:3000'));
+
+function authenticate(req, res, next) {
+    var body = req.body;
+    if (!body.username || !body.password) {
+        res.status(400).end('Must provide username or password');
+    }
+    if (body.username !== user.username || body.password !== user.password) {
+        res.status(401).end('Username or password incorrect');
+    }
+    next();
+}
