3678145b98f7d471c22a6e363ebbc686f54a4923
Kevin Gosse <gossekevin1@gmail.com>
2016-01-08 21:55:08 +0100

    add an auth interceptor

--- public/main.js
+++ public/main.js
@@ -4,7 +4,9 @@
 
 import angular from "angular";
 
-var app = angular.module("app", []);
+var app = angular.module('app', [], function config($httpProvider) {
+    $httpProvider.interceptors.push('AuthInterceptor');
+});
 
 app.constant('API_URL', 'http://localhost:3000');
 
@@ -12,6 +14,7 @@ app.controller('MainCtrl', function(RandomUserFactory, UserFactory){
     var vm = this;
     vm.getRandomUser = getRandomUser;
     vm.login = login;
+    vm.logout = logout;
 
     function getRandomUser(){
         RandomUserFactory.getUser().then((response)=>{
@@ -25,25 +28,38 @@ app.controller('MainCtrl', function(RandomUserFactory, UserFactory){
         }, handleError);
     }
 
+    function logout() {
+        UserFactory.logout();
+        vm.user = null;
+    }
+
     function handleError(response){
         alert('Error' + response.data);
     }
 });
 
-app.factory('UserFactory', function($http, API_URL){
+app.factory('UserFactory', function($http, API_URL, AuthTokenFactory){
     'use strict';
 
     return {
-        login
+        login,
+        logout
     };
 
     function login(username, password){
         return $http.post(API_URL + '/login', {
             username,
             password
+        }).then(function success(response) {
+            AuthTokenFactory.setToken(response.data.token);
+            return response;
         });
     }
-})
+
+    function logout() {
+        AuthTokenFactory.setToken();
+    }
+});
 
 app.factory('RandomUserFactory', function($http, API_URL){
     return {
@@ -52,6 +68,46 @@ app.factory('RandomUserFactory', function($http, API_URL){
 
     function getUser(){
         return $http.get(API_URL + '/random-user');
+    }
+});
+
+app.factory('AuthTokenFactory', function AuthTokenFactory($window) {
+    'use strict';
+    var store = $window.localStorage;
+    var key = 'auth-token';
+
+    return {
+        getToken,
+        setToken
+    };
+
+    function getToken() {
+        return store.getItem(key);
+    }
+
+    function setToken(token) {
+        if (token) {
+            store.setItem(key, token);
+        } else {
+            store.removeItem(key);
+        }
+    }
+
+});
+
+app.factory('AuthInterceptor', function AuthInterceptor(AuthTokenFactory) {
+    'use strict';
+    return {
+        request: addToken
+    };
+
+    function addToken(config) {
+        var token = AuthTokenFactory.getToken();
+        if (token) {
+            config.headers = config.headers || {};
+            config.headers.Authorization = 'Bearer ' + token;
+        }
+        return config;
     }
 });
 
